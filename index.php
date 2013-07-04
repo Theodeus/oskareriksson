dc69a6a60ae4b292b6f1f142c2d691596ee2c69f
Oskar Eriksson <oskar.det@gmail.com>
2013-07-04 18:18:23 +0200

    No PHP, apparently

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-echo("I can use PHP");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Oskar Eriksson - Web Developer</title>
+    <style>
+    </style>
+</head>
+<body>
+    <p>Turns out you can't use PHP. Good to know</p>
+</body>
+</html>
